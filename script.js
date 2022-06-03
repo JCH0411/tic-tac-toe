9c5ed72a3ec7861c1b4b1ff894be7db412f75f9a
John <john.houston0411@gmail.com>
2022-06-03 10:41:01 -0500

    script modified

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const player = (marker) =>{
   const plays = [];
   const {arr} = gameBoard();
   const playerChoice = (input) => {
-     plays.push(input);
+     // plays.push(input);
     }
   const print = () =>{
-    
+
   }
   return {marker, playerChoice, print};
 }
 
 
 const player1 = player('x');
-console.log(player1 instanceof gameBoard)
+const player2 = player('o')
