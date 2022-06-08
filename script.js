149bc268bf4bbf4e031d7e3a337ab6a978c38694
john <john.houston0411@gmail.com>
2022-06-07 19:47:30 -0500

    script

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+//winning choices
+const winningOptions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 //module to control flow of the game
 const game = (() => {
   let humanPlayer = null;
   let totalPlays = null;
   let ai = null;
+  let winningCells = winningOptions.length;
 
   const ready = () => {
     if (humanPlayer != null)
@@ -18,11 +30,20 @@ const game = (() => {
   const getAiMarker = () => {
     return ai.returnMarker()
   }
-  const getGameSize = () =>{
+  const getGameSize = () => {
     return totalPlays.sizeOfGame();
   }
-  const checkForWinner = () => {
+  const checkForAiWinner = () => {
 
+  }
+  const checkForHumanWinner = () =>{
+
+      for(let x = 0; x < winningCells; x++){
+         for(let i = 0; i < 3; i++){
+           if(winningOptions[x][i] === humanPlayer.checkValues())
+
+         }
+      }
   }
 
   const addHumanPlayer = (human) => {
@@ -39,6 +60,7 @@ const game = (() => {
       document.querySelectorAll(".mark")[x].disabled = true;
     }
   }
+
 
   const storeAiSelect = (input) => {
     ai.addPlayerChoice(input);
@@ -57,8 +79,10 @@ const game = (() => {
     storePlayerSelect,
     storeAiSelect,
     getAiMarker,
-    getGameSize
-    };
+    getGameSize,
+    checkForAiWinner,
+    checkForHumanWinner
+  };
 })();
 
 //player select of either X or O
@@ -81,7 +105,10 @@ document.querySelector(".game").addEventListener("click", (event) => {
       playerSelect.textContent = game.getHumanMarker();
       event.target.append(playerSelect);
       game.storePlayerSelect(event.target.id);
-      setTimeout(() => {aiPlay()}, "900")
+      game.checkForAiWinner();
+      setTimeout(() => {
+        aiPlay()
+      }, "900")
 
     } else { //need to figure out this else
 
@@ -96,56 +123,22 @@ document.querySelector(".game").addEventListener("click", (event) => {
 
 //function to use ai to play
 const aiPlay = () => {
-  let ai =true;
-  let rand = null;
+
   let gameboardNodes = document.querySelectorAll(".gameboard");
   len = document.querySelectorAll(".gameboard").length;
-   while(ai){
-     rand = Math.floor(Math.random() * 9)
-     // if(game.getGameSize() >= 8){
-     //   ai = false;
-     // }
-     if(!gameboardNodes[rand].hasChildNodes() && game.getGameSize() >= 8){
-       let aiSelect = document.createElement("p");
-           aiSelect.classList.add("game-cell");
-           aiSelect.textContent = game.getAiMarker();
-           gameboardNodes[rand].append(aiSelect);
-           game.storeAiSelect(gameboardNodes[rand].id);
-           ai = false;
-     }
-   }
+  let rand = Math.floor(Math.random() * 9);
+  if (!gameboardNodes[rand].hasChildNodes() && game.getGameSize() <= 8) {
+    let aiSelect = document.createElement("p");
+    aiSelect.classList.add("game-cell");
+    aiSelect.textContent = game.getAiMarker();
+    gameboardNodes[rand].append(aiSelect);
+    game.storeAiSelect(gameboardNodes[rand].id);
+  }
 
-  // while (ai) {
-  //   rand = Math.floor(Math.random() * 9)
-  //
-  //   if( game.getGameSize() === 8 ){
-  //     ai = false;
-  //   }
-  //     if (!gameboardNodes[rand].hasChildNodes()) {
-  //       console.log(gameboardNodes[rand])
-  //       let aiSelect = document.createElement("p");
-  //       aiSelect.classList.add("game-cell");
-  //       aiSelect.textContent = game.getAiMarker();
-  //       gameboardNodes[rand].append(aiSelect);
-  //       game.storeAiSelect(gameboardNodes[rand].id);
-  //       ai = false;
-  //     }
-  //
-  // }
 }
 
 
-//winning choices
-const winningOptions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
+
 
 //gameboard object
 const gameBoard = () => {
@@ -162,7 +155,7 @@ const gameBoard = () => {
     }
 
   }
-  const sizeOfGame = () =>{
+  const sizeOfGame = () => {
 
     return game.length;
   }
@@ -176,9 +169,6 @@ const gameBoard = () => {
 // player object
 const player = (marker) => {
   const plays = [];
-  // const {
-  //   arr
-  // } = gameBoard();
 
   const addPlayerChoice = (input) => {
     plays.push(input);
@@ -191,6 +181,7 @@ const player = (marker) => {
   return {
     returnMarker,
     addPlayerChoice,
+    plays
 
   };
 }
